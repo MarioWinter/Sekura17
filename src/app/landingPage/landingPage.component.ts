031eb4a2bdca9bb353286f31a147f1c823600d71
Mario Winter <mariowinter.sg@gmail.com>
2024-05-20 23:24:27 +0200

    HTML code navbar added

--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
-	selector: 'app-landingPage', // ist ein Selektor um es im anderen HTML aufzurufen
-	standalone: true,
-	imports: [], // Wenn andere Componente oder Services benutzt werden sollen
-	template: /*html*/ `
-		<section>
-			<h1 class="fontRaleway">SAKURA RAMEN</h1>
-			<h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
-		</section>
-	`, // ist die grundvorlage der Komponent HTML
-	styleUrls: ['./landingPage.component.scss'], // Vorlage des Style der Template
+    selector: "app-landingPage", // ist ein Selektor um es im anderen HTML aufzurufen
+    standalone: true,
+    imports: [NavbarComponent], // Wenn andere Componente oder Services benutzt werden sollen
+    template: /*html*/ `
+        <section>
+            <app-navbar></app-navbar>
+            <h1 class="fontRaleway">SAKURA RAMEN</h1>
+            <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
+        </section>
+    `, // ist die grundvorlage der Komponent HTML
+    styleUrls: ["./landingPage.component.scss"], // Vorlage des Style der Template
 })
 export class LandingPageComponent {}
