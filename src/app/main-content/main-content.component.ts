2d9c86f457696d4c48685c3962b54db4eaa8f8b0
Mario Winter <mariowinter.sg@gmail.com>
2024-05-25 20:05:36 +0200

    App find us at component added

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, importProvidersFrom } from "@angular/core";
 import { LandingPageComponent } from "./landing-page/landingPage.component";
 import { HowToOrderComponent } from "./how-to-order/how-to-order.component";
 import { OurMenuComponent } from "./our-menu/our-menu.component";
+import { FindUsAtComponent } from "./find-us-at/find-us-at.component";
 
 @Component({
     selector: "app-main-content",
     standalone: true,
-    imports: [LandingPageComponent, HowToOrderComponent, OurMenuComponent],
+    imports: [LandingPageComponent, HowToOrderComponent, OurMenuComponent, FindUsAtComponent],
     templateUrl: "./main-content.component.html",
     styleUrl: "./main-content.component.scss",
 })
