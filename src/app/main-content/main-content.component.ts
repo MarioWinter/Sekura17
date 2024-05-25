6496a9b7a81e57278ad06222d320d35c9e5197f5
Mario Winter <mariowinter.sg@gmail.com>
2024-05-25 20:42:17 +0200

    footer section added

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { LandingPageComponent } from "./landing-page/landingPage.component";
 import { HowToOrderComponent } from "./how-to-order/how-to-order.component";
 import { OurMenuComponent } from "./our-menu/our-menu.component";
 import { FindUsAtComponent } from "./find-us-at/find-us-at.component";
+import { FooterComponent } from "./../shared/components/footer/footer.component";
 
 @Component({
     selector: "app-main-content",
     standalone: true,
-    imports: [LandingPageComponent, HowToOrderComponent, OurMenuComponent, FindUsAtComponent],
+    imports: [LandingPageComponent, HowToOrderComponent, OurMenuComponent, FindUsAtComponent, FooterComponent],
     templateUrl: "./main-content.component.html",
     styleUrl: "./main-content.component.scss",
 })
